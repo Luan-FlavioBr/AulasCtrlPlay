d34e49153a6fffdf8281e5b5d894fbc03b00e23a
Luan Flavio <luanflavio.bm@gmail.com>
2024-07-15 17:12:33 -0300

    [feat] Def Alterar produto

--- Projeto/main.py
+++ Projeto/main.py
@@ -43,7 +43,35 @@ def apagar_produto():
     with open("produtos.txt", "w") as a:
         for linha in lista_temporaria:
             a.write(f"{linha[0]},{linha[1]},{linha[2]}\n")
+
+
+def alterar_produtos():
+    listar_produtos()
+    print("-"*50)
+    cod_produto = input("Digite o código do produto que você gostaria de alterar: ")
+    lista_temporaria = list()
+
+    # Adiciona todos os dados em uma nova lista
+    with open("produtos.txt", "r") as a:
+        for linha in a:
+            linha = linha.replace("\n", "")
+            linha = linha.split(",")
+            lista_temporaria.append(linha)
     
+    # Procura pela lista o código informado
+    for i, produto in enumerate(lista_temporaria):
+
+        # Se achar o código, altera as informações
+        if produto[0] == cod_produto:
+            nome_produto_novo = input("Digite o novo nome do produto: ")
+            valor_produto_novo = input("Digite o novo valor do produto: R$")
+            produto[1] = nome_produto_novo
+            produto[2] = valor_produto_novo
+
+    # Reescreve todo o arquivo com a nova lista
+    with open("produtos.txt", "w") as a:
+        for linha in lista_temporaria:
+            a.write(f"{linha[0]},{linha[1]},{linha[2]}\n")
 
 while True:
     print(f"{' Mercadinho do Zé ':-^50}\n")
@@ -60,7 +88,11 @@ while True:
     elif resposta == "2":
         os.system("cls")
         listar_produtos()
+    elif resposta == "3":
+        os.system("cls")
+        alterar_produtos()
     elif resposta == "4":
         apagar_produto()
+        os.system("cls")
     elif resposta == "5":
         break
